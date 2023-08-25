60a83d69e702e980abc46316f7f4d1425b1c9569
Jhonatan <jhon.devbr@gmail.com>
2023-08-24 23:58:21 -0300

    Atualizei os métodos falta, fazer a conecxão com BD e gerar o recibo

--- SistemaParaDesktop/Ferias.cs
+++ SistemaParaDesktop/Ferias.cs
@@ -425,7 +425,7 @@ namespace SistemaParaDesktop
                         Console.WriteLine();
                     }
                     break;
-
+                // Aqui vou precisar enviar as informações das variaveis para o banco de dados.
                 default:
                     Console.WriteLine("- Error, Opção invalida, selecione entre as opções.");
                     Console.WriteLine("- Dígite qualquer coisa para continuar.");
@@ -436,9 +436,8 @@ namespace SistemaParaDesktop
             }
         }
 
-        public double OpcaoDeFerias()
+        public double CalculoDasFerias()
         {
-            //Aqui eu vou mostrar ao usuario as opções de férias que ele possui
             double ferias = 0;
             int escolha;
 
@@ -449,7 +448,6 @@ namespace SistemaParaDesktop
                 Console.WriteLine("- Para periodo completo dígite [1]");
                 Console.WriteLine("- Para 2 periodos dígite [2]");
                 Console.WriteLine("- Para 3 periodos dígite [3]");
-                Console.WriteLine("- Para a venda das férias dígite [4]");
                 Console.Write("- Escolha...: ");
                 escolha = int.Parse(Console.ReadLine());
                 Console.WriteLine();
@@ -457,22 +455,17 @@ namespace SistemaParaDesktop
                 {
                     case 1:
                         Console.WriteLine("- O funcionário solicitou as férias por completo");
-                        ferias = CalcularPeriodoCompletoDeDeFerias();
+                        ferias = FormulaPeriodoCompletoDeDeFerias();
                         break;
 
                     case 2:
                         Console.WriteLine("- O fucnionário solicitou as férias em 2 periodos");
-                        ferias = CalcularFeriasEmDoisPeriodos();
+                        ferias = FormulaFeriasEmDoisPeriodos();
                         break;
 
                     case 3:
                         Console.WriteLine("- O fucnionário solicitou as férias em 3 periodos");
-                        ferias = CalcularFeriasEmTresPeriodos();
-                        break;
-
-                    case 4:
-                        Console.WriteLine("- O funcionário solicitou a venda das férias");
-                        ferias = CalcularVendaDasFerias();
+                        ferias = FormulaFeriasEmTresPeriodos();
                         break;
 
                     default:
@@ -489,63 +482,108 @@ namespace SistemaParaDesktop
             return DiasGozados;
         }
 
-        public double CalcularPeriodoCompletoDeDeFerias()
+        public double FormulaPeriodoCompletoDeDeFerias()
         {
-            // Depois vou fazer este metodo buscar os valores de INSS e IRRF ja calculados para serem utilizados aqui.
-            double salarioBase, umTercoSalarioBase, salarioBrutoFerias, inss, irrf, pensao = 300, dependente = 400, salarioLiquidoFerias;
-
-            // Aqui eu vou perguntar se ele vendeu as ferias e chamar o método de calculo de venda das férias.
-            Console.WriteLine("- Terceiro passo para calcular as férias:");
-            Console.WriteLine();
+            // Vou precisar passar os valores do INSS e IRRF por parametros.
+            double salarioBase, umTercoSalarioBase, salarioBrutoFerias, inss, irrf, pensao = 300, dependente = 400, salarioLiquidoFerias = 0;
+            int dias;
+            char venda;
 
             Console.Write("- Informe o salário base do funcionario: R$ ");
             salarioBase = double.Parse(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("- O funcionario realizou a venda das férias? Para SIM [S], para NÃO [N].");
+                Console.Write("- Venda...: ");
+                venda = char.Parse(Console.ReadLine().ToUpper());
+                Console.WriteLine();
+                if (venda == 'S')
+                {
+                    do
+                    {
+                        Console.WriteLine("- Informe a quantidade de dias vendidos. MÁXIMO 10 dias.");
+                        dias = int.Parse(Console.ReadLine());
+                    } while (dias > 10);
+                    AbonoPecuniario = FormulaVendaDasFerias(salarioBase, dias);
+                    UmTercoDoAbonoPecuniario = AbonoPecuniario / 3;
+                    umTercoSalarioBase = salarioBase / 3;
+                    salarioBrutoFerias = salarioBase + umTercoSalarioBase + AbonoPecuniario + UmTercoDoAbonoPecuniario;
+                    BrutoDeFerias = salarioBrutoFerias;
+                    DiasGozados = 30 - dias;
+                    Console.WriteLine();
 
-            umTercoSalarioBase = salarioBase / 3;
-            salarioBrutoFerias = salarioBase + umTercoSalarioBase;
-            BrutoDeFerias = salarioBrutoFerias;
-            DiasGozados = 30;
-            Console.WriteLine();
+                    Console.WriteLine($"- O salario bruto das férias é de: R$ {salarioBrutoFerias:f2}");
+                    Console.WriteLine();
 
-            Console.WriteLine($"- O salario bruto das férias é de: R$ {salarioBrutoFerias:f2}");
-            Console.WriteLine();
+                    inss = FolhaDePagamento.FormulaDoInss(salarioBrutoFerias);
+                    InssFerias = inss;
+                    irrf = FolhaDePagamento.FormulaDoIrrf(salarioBrutoFerias, inss, pensao, dependente);
+                    IrrfFerias = irrf;
+                    salarioLiquidoFerias = salarioBrutoFerias - inss - irrf;
+                    Console.WriteLine();
 
-            inss = FolhaDePagamento.FormulaDoInss(salarioBrutoFerias);
-            InssFerias = inss;
-            irrf = FolhaDePagamento.FormulaDoIrrf(salarioBrutoFerias, inss, pensao, dependente);
-            IrrfFerias = irrf;
-            salarioLiquidoFerias = salarioBrutoFerias - inss - irrf;
-            Console.WriteLine();
+                    Console.WriteLine($"- O valor das férias que o funcionario ira receber é de: R$ {salarioLiquidoFerias:f2} referente a {DiasGozados} dias gozados.");
+                }
+                else if (venda == 'N')
+                {
+                    umTercoSalarioBase = salarioBase / 3;
+                    salarioBrutoFerias = salarioBase + umTercoSalarioBase;
+                    BrutoDeFerias = salarioBrutoFerias;
+                    DiasGozados = 30;
+                    Console.WriteLine();
 
-            Console.WriteLine($"- O valor das férias que o funcionario ira receber é de: R$ {salarioLiquidoFerias:f2}");
+                    Console.WriteLine($"- O salario bruto das férias é de: R$ {salarioBrutoFerias:f2}.");
+                    Console.WriteLine();
+
+                    inss = FolhaDePagamento.FormulaDoInss(salarioBrutoFerias);
+                    InssFerias = inss;
+                    irrf = FolhaDePagamento.FormulaDoIrrf(salarioBrutoFerias, inss, pensao, dependente);
+                    IrrfFerias = irrf;
+                    salarioLiquidoFerias = salarioBrutoFerias - inss - irrf;
+                    Console.WriteLine();
+
+                    Console.WriteLine($"- O valor das férias que o funcionario ira receber é de: R$ {salarioLiquidoFerias:f2} referente a {DiasGozados} dias gozados.");
+                }
+                else
+                {
+                    Console.WriteLine("- Error, Opção invalida, selecione entre as opções.");
+                    Console.WriteLine("- Dígite qualquer coisa para continuar.");
+                    Console.ReadKey();
+                    Console.WriteLine("¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨ REFAÇA A OPERAÇÃO ¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨");
+                    Console.WriteLine();
+                }
+            } while (venda != 'S' && venda != 'N');
             return salarioLiquidoFerias;
         }
         
-        public double CalcularFeriasEmDoisPeriodos()
+        public double FormulaFeriasEmDoisPeriodos()
         {
             double salarioBase, umTercoValorFerias, salarioBrutoFerias, inss, irrf, pensao = 300, dependente = 400, valorFerias, diaria, salarioLiquidoFerias;
             int dias;
-
+            
+            // Vou precisar passar os valores do INSS e IRRF por parametros.
             // Vou precisar puxar do banco qual é o periodo que esta sendo calculado e qual o usuario ja retirou. Tambem preciso colocar um limite para 30 dias de férias.
             // Aqui eu vou perguntar se ele vendeu as ferias e chamar o método de calculo de venda das férias.
-            Console.WriteLine("- Terceiro passo para calcular as férias:");
-            Console.WriteLine();
+            Console.Write("- Informe o salário base do funcionario: R$ ");
+            salarioBase = double.Parse(Console.ReadLine());
 
             //aqui eu vou puxar esta informação do banco de dados.
             Console.Write("- Quantos dias o funcionario solicitou de férias: ");
             dias = int.Parse(Console.ReadLine());
 
-            Console.Write("- Informe o salário base do funcionario: R$ ");
-            salarioBase = double.Parse(Console.ReadLine());
-
+            /* Aqui vou criar uma condição buscando dados no BD para impedir a venda das férias se ele ja tiver vendido os 10 dias
+             * ou permitir que ele venda os dias restantes caso ele não tenha vendido todos os 10 dias.
+             Tambem vou buscar no BD a quantidade de dias que ele optou por tirar de férias e o mes para fazer o calculo.*/
+            
+            // Por enquanto vou deixar assim.
             diaria = salarioBase / 30;
-            valorFerias = (double) dias * diaria;
+            valorFerias = dias * diaria;
             umTercoValorFerias = valorFerias / 3;
             salarioBrutoFerias = valorFerias + umTercoValorFerias;
             BrutoDeFerias = salarioBrutoFerias;
             Console.WriteLine();
 
-            Console.WriteLine($"- O salario bruto das férias é de: R$ {salarioBrutoFerias:f2}");
+            Console.WriteLine($"- O salario bruto das férias é de: R$ {salarioBrutoFerias:f2}.");
             Console.WriteLine();
 
             inss = FolhaDePagamento.FormulaDoInss(salarioBrutoFerias);
@@ -559,27 +597,29 @@ namespace SistemaParaDesktop
             return salarioLiquidoFerias;
         }
         
-        public double CalcularFeriasEmTresPeriodos()
+        public double FormulaFeriasEmTresPeriodos()
         {
-            // Ese método esta imcompleto preciso terminar
+            // Vou precisar passar os valores do INSS e IRRF por parametros.
             double salarioBase, umTercoValorFerias, salarioBrutoFerias, inss, irrf, pensao = 300, dependente = 400, valorFerias, diaria, salarioLiquidoFerias;
-            int dias, primeiroPeriodo, segundoPerido, terceiroPeriodo;
+            int dias;
+            string periodo;
+
             // Aqui eu vou perguntar se ele vendeu as ferias e chamar o método de calculo de venda das férias.
-            Console.Write("- Quantos dias o fucionario solicitou para o 1º periodo: ");
-            primeiroPeriodo = int.Parse(Console.ReadLine());
-            Console.Write("- Quantos dias o fucionario solicitou para o 2º periodo: ");
-            segundoPerido = int.Parse(Console.ReadLine());
-            Console.Write("- Quantos dias o fucionario solicitou para o 3º periodo: ");
-            terceiroPeriodo = int.Parse(Console.ReadLine());
+            // A variavel dias irei usar para puxar os dias do periodo no salvos no BD.
+            // A variavel periodo irei usar para puxar o mes do periodo solicitado salvo no BD.
 
             Console.Write("- Quantos dias o funcionario solicitou de férias: ");
             dias = int.Parse(Console.ReadLine());
-
+            Console.Write($"- Os {dias} dias que funcionario solicitou de férias se referen a qual periodo: ");
+            periodo = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine($"- O funcionário solicitou {dias} para o {periodo}");
+            
             Console.Write("- Informe o salário base do funcionario: R$ ");
             salarioBase = double.Parse(Console.ReadLine());
 
             diaria = salarioBase / 30;
-            valorFerias = (double)dias * diaria;
+            valorFerias = dias * diaria;
             umTercoValorFerias = valorFerias / 3;
             salarioBrutoFerias = valorFerias + umTercoValorFerias;
             BrutoDeFerias = salarioBrutoFerias;
@@ -599,63 +639,14 @@ namespace SistemaParaDesktop
             return salarioLiquidoFerias;
         }
         
-        public double CalcularVendaDasFerias()
+        public double FormulaVendaDasFerias(double salario, int dias)
         {
-            // Aqui eu vou calcular o valor dos dias vendidos das ferias recebendo a quantidade de dias por parametros e retornando o valor da venda.
-            // Preciso colocar um limite para 30 dias de férias
-            double salarioBase, diaria, valorFerias, umTercoValorFerias, abonoPecuniario, umTercoAbonoPecuniaro, salarioBrutoFerias, baseDoInssIrrf, inss, irrf, pensao = 300, dependente = 400, salarioLiquidoFerias;
-            int diasVendidos, restante;
+            double diaria, abonoPecuniario;
 
-            Console.WriteLine("- Venda das férias.");
-            Console.WriteLine();
-            Console.Write("- Informe quantos dias o funcionario deseja vender. Máximo de 10 dias permitidos: ");
-            diasVendidos = int.Parse(Console.ReadLine());
-            DiasVendidos = diasVendidos;
-            Console.Write("- Informe o salário base do funcionario: R$ ");
-            salarioBase = double.Parse(Console.ReadLine());
-            Console.WriteLine();
-
-            restante = 30 - diasVendidos;
-            DiasGozados = restante;
-
-            diaria = salarioBase / 30; // Aqui eu vou descobri qual o valor que o funcionario ganha por dia.
-            valorFerias = (double) restante * diaria; // Aqui eu vou calcular quanto ele vai receber de férias .
-            umTercoValorFerias = valorFerias / 3; // Aqui eu calculo 1/3 do valor que ele vai receber das férias.
-
-            abonoPecuniario = (double) diasVendidos * diaria; // Aqui eu calculo o valor que ele vai receber pelos dias vendidos.
-            AbonoPecuniario = abonoPecuniario;
-
-            umTercoAbonoPecuniaro = abonoPecuniario / 3; // Aqui eu calculo 1/3 do valor da venda das férias
-            UmTercoDoAbonoPecuniario = umTercoAbonoPecuniaro;
-
-            salarioBrutoFerias = valorFerias + umTercoValorFerias + abonoPecuniario + umTercoAbonoPecuniaro; // Aqui é o valor bruto que ele tem para receber
-            BrutoDeFerias = salarioBrutoFerias;
-            Console.WriteLine();
-
-            Console.WriteLine($"- O salario bruto das férias é de: R$ {salarioBrutoFerias:f2}");
-            Console.WriteLine();
-
-            baseDoInssIrrf = valorFerias + umTercoValorFerias; // Aqui eu calculo o valor sobre o qual vai ser descontado o INSS e IRRF pois ele não é calculado sobre o abono pecuniario nem o valor de 1/3 do mesmo.
-
-            inss = this.FolhaDePagamento.FormulaDoInss(baseDoInssIrrf);
-            InssFerias = inss;
-            irrf = this.FolhaDePagamento.FormulaDoIrrf(salarioBrutoFerias, inss, pensao, dependente);
-            IrrfFerias = irrf;
-
-            salarioLiquidoFerias = salarioBrutoFerias - inss - irrf;
-            Console.WriteLine();
-
-            Console.WriteLine($"- Valor dos 20 dias de férias: R$ {valorFerias:f2}");
-            Console.WriteLine($"- Valor de 1/3 dos 20 dias: R$ {umTercoValorFerias:f2}");
-            Console.WriteLine();
-            Console.WriteLine($"- Valor da venda dos dias: R$ {abonoPecuniario:f2}");
-            Console.WriteLine($"- Valor de 1/3 do abono pecuniario: R$ {umTercoAbonoPecuniaro:f2}");
-            Console.WriteLine();
-            Console.WriteLine($"- Desconto do INSS: R$ {inss:f2}");
-            Console.WriteLine($"- Desconto do IRRF: R$ {irrf:f2}");
-            Console.WriteLine();
-            Console.WriteLine($"- Valor líquido a reber de férias: R$ {salarioLiquidoFerias:f2}");
-            return salarioLiquidoFerias;
+            diaria = salario / 30;
+            abonoPecuniario = dias * diaria;
+            UmTercoDoAbonoPecuniario = abonoPecuniario / 3;
+            return abonoPecuniario;
         }
 
         public void GerarReciboDeFerias()
