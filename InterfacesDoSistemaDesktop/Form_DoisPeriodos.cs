267dfa1b7c9da0315f8b5ee966caffbffc365926
Jhonatan <jhon.devbr@gmail.com>
2023-08-26 22:31:26 -0300

    Adicionei as telas de Férias completas e em dois períodos

---
+++ InterfacesDoSistemaDesktop/Form_DoisPeriodos.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InterfacesDoSistemaDesktop
+{
+    public partial class Form_DoisPeriodos : Form
+    {
+        public Form_DoisPeriodos()
+        {
+            InitializeComponent();
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void rdbSim_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdbSim.Checked == true)
+            {
+                lblQuantidade.Enabled = true;
+                txtQuantidade.Enabled = true;
+            }
+            else if (rdbSim.Checked == false)
+            {
+                lblQuantidade.Enabled = false;
+                txtQuantidade.Enabled = false;
+            }
+        }
+
+        private void btnAgendar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gpbDoisPeriodos_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConcluir_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("As férias foram agendadas com sucesso!", "Atividade concluida");
+            Close();
+        }
+
+       
+    }
+}
---
+++ InterfacesDoSistemaDesktop/InterfacePrincipal.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InterfacesDoSistemaDesktop
+{
+    public partial class InterfacePrincipal : Form
+    {
+        public InterfacePrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void agendarFériasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            /* Eu tentei fazer a janela fehcar essa janela e abrir a outra sem ficar com duas janelas abertas mas não consegui
+             depois eu tento fazer isso, por enquanto vou fazer a interface do formulario que o professor solicitou.
+            InterfaceAgendarFerias ObjInterfaceAgendarFerias = new InterfaceAgendarFerias();
+            ObjInterfaceAgendarFerias.Show();
+            */
+        }
+
+        private void períodoCompletoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form_PeriodoCompleto ObjFormPeriodoCompleto = new Form_PeriodoCompleto();
+            ObjFormPeriodoCompleto.Show();
+        }
+
+        private void doisPeríodosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form_DoisPeriodos form_DoisPeriodos = new Form_DoisPeriodos();
+            form_DoisPeriodos.Show();
+        }
+
+        private void trêsPeríodosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+
+        
+    }
+}
